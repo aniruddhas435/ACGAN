6fd77282f70d5fd7ba4f796dc594fe2be21200c7
aniruddhas435 <47499540+aniruddhas435@users.noreply.github.com>
2019-04-02 15:45:00 +0530

    Update write_numbers.py

--- write_numbers.py
+++ write_numbers.py
@@ -1,25 +1,45 @@
+import pandas as pd
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from keras.models import load_model
+import keras
+from keras.layers import Dense, Flatten, Conv2D, UpSampling2D, BatchNormalization, Input, LeakyReLU, Activation, Dropout, Reshape, Conv2DTranspose
+from keras.optimizers import *
+from keras.initializers import RandomNormal
+from keras.models import Model
+from IPython import display
+from tqdm import tqdm
+import random
+from matplotlib import pyplot as plt
 
+generator = keras.models.load_model('generator\model.h5')
+discriminator = keras.models.load_model('discriminator\model-4000.h5')
 
-generator = load_model('generator\model-4000.h5')
+string = input('Enter the digits with spaces in between: ')
+digits = string.split(' ')
 
-def write_number(number):
-    noise = np.random.uniform(-1.0, 1.0, size = [1, 100])
-    n = len(number)
-    image = generator.predict([noise, np.array([int(number[0])])])
+count = 0
+images = []
 
-    for i in range(1, n):
-        noise = np.random.uniform(-1.0, 1.0, size = [1, 100])
-        curr_digit = int(number[i])
-        curr_image = generator.predict([noise, np.array([curr_digit])])
-        image = np.concatenate((image, curr_image), axis = 2)
-    
-    plt.imshow(image[0, :, :, 0], cmap = 'gray')
-    plt.show()
-    plt.close()
+noise = np.random.uniform(-1.0, 1.0, size = [1, 100])
+for i in tqdm(range(len(digits) - 1)):
+    for j in range(10):
+        digit = np.zeros((1, 10))
+        digit[:, int(digits[i])] = (10 - j) * 0.1
+        digit[:, int(digits[i + 1])] = j * 0.1
+        image = generator.predict([noise, digit])
+        plt.imshow(image[0, :, :, 0])
+        plt.savefig('image.png'.format(count))
+        images.append(cv2.imread('image.png'))
+        plt.close()
+        count += 1
 
+height, width, layer = images[0].shape
 
-while True:
-    write_number(str(input('Enter the number: ')))
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter('twining_digits.avi', fourcc, 4.0, (width, height))
+
+for i in range(len(images)):
+    video.write(images[i])
+
+cv2.destroyAllWindows()
+video.release()
